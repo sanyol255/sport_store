1be356195523126b7ca41e26e405e68583e89729
olkov <oleksandr.kovalchuk@smile-ukraine.com>
2022-05-16 16:00:34 +0300

    Adding block for editing product info

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from "@angular/forms";
 
 import { AppComponent } from './app.component';
 import { StoreInfoComponent } from './store-info/store-info.component';
@@ -12,7 +13,8 @@ import { ProductsComponent } from './products/products.component';
     ProductsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
