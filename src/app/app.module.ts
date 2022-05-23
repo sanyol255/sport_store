676892ea0a367d2bf94349d800be0ff0786c15e8
olkov <oleksandr.kovalchuk@smile-ukraine.com>
2022-05-23 10:50:47 +0300

    Moving product detail functionality to dedicated component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { FormsModule } from "@angular/forms";
 import { AppComponent } from './app.component';
 import { StoreInfoComponent } from './store-info/store-info.component';
 import { ProductsComponent } from './products/products.component';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     StoreInfoComponent,
-    ProductsComponent
+    ProductsComponent,
+    ProductDetailComponent
   ],
   imports: [
     BrowserModule,
